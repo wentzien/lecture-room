4de02586441b4bab6bb9fd5334ac7ac5dbe123fa
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2020-10-16 20:02:27 +0200

    update server

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/', function(req, res){
+    res.sendFile('index.html');
 });
 
 const port = process.env.PORT || 4000;
