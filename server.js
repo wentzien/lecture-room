fe4cdd98e1ad5278313910966a3fdc8e605126e3
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2020-10-17 12:34:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
-
-app.get('/', function(req, res){
-    res.sendFile('index.html');
-});
+app.use('/', express.static(__dirname + '/public'));
+app.use('/lecture-room', express.static(__dirname + '/public'));
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`App listening on port ${port}...`));
